5656483a52e9847f1d90d835c89fcac5ef1b5cfe
Agnieszka Rzeszuto <aga.rz@tlen.pl>
2014-05-15 21:51:44 +0200

    pierwsze testy

--- spec/controllers/gists_controller_spec.rb
+++ spec/controllers/gists_controller_spec.rb
@@ -2,10 +2,63 @@ require 'spec_helper'
 
 describe GistsController do
   describe "GET #index" do
-    it "responds successfully with an HTTP 200 status code" do
+    it "responds successfully index page" do
       get :index
       expect(response).to be_success
       expect(response.status).to eq(200)
     end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "load all of the gists into @gists" do
+      gist1, gist2 = Gist.create!, Gist.create!
+      get :index
+      expect(assigns(:gists)).to match_array([gist1, gist2])
+    end
   end
+
+  describe "GET #new" do
+    it "responds successfully new gist page" do
+      get :new
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
+
+=begin
+  describe "GET #edit" do
+    it "responds successfully edit page" do
+      get :edit
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the edit template" do
+      get :edit
+      expect(response).to render_template("edit")
+    end
+  end
+=end
+
+  describe "GET #search" do
+    it "responds successfully search page" do
+      get :search
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the search tepmlate" do
+      get :search
+      expect(response).to render_template("search")
+    end
+  end
+
 end
