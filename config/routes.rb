bfa802bd8262f8fa8b54fbdace78ff93be4fa646
ddstenka <d.d.stenka@gmail.com>
2014-05-19 11:13:31 +0200

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 MyGists::Application.routes.draw do
   get "gists/search"
+  get "static_pages/login_required"
+  get "static_pages/login_failed"
 
   resources :gists
   
   root :to => "gists#index"
 
+=begin
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: redirect('/')
   match 'signout', to: 'sessions#destroy', as: 'signout'
+=end
 
+  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: 'static_pages#login_failed', via: [:get, :post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 end
