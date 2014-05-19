16101bc33e915e74eb0b3096d69efbe73bb47720
kpawel-29 <pakadlubowski@gmail.com>
2014-05-19 21:00:47 +0200

    add test 'check display autor in #show'

--- spec/views/gists/show.html.erb_spec.rb
+++ spec/views/gists/show.html.erb_spec.rb
@@ -5,7 +5,7 @@ describe "gists/show.html.erb" do
   it "check display autor" do
     @gist = Gist.create(snippet: "The title", lang: "css", description: "Desc something", autor: "test")
     visit gist_path('1')
-    expect(page).to have_content("autor")
+    expect(page).to have_content("Autor")
   end
 
 
