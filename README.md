ca0bf1096329e768ed7035502ac2b856acb3f0e6
kpawel-29 <pakadlubowski@gmail.com>
2014-05-21 15:36:55 +0200

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [![Build Status](https://travis-ci.org/kpawel-29/Disco-w-rytmie-polo.svg?branch=master)](https://travis-ci.org/kpawel-29/Disco-w-rytmie-polo)
-<pre>Deadline oddawania projektu przesunięty na 24.05</pre>
+<pre>Deadline oddawania projektu przesunięty na 23.05</pre>
 
 Disco-w-rytmie-polo
 ===================
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.name = auth.extra.raw_info.login
-      user.oauth_token = auth.credentials.token
+      #user.name = auth.extra.raw_info.login
+      #user.oauth_token = auth.credentials.token
 =begin
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
 =end
