a542eceeb1d03312c5f50a803b7ee5c9570370ee
ddstenka <d.d.stenka@gmail.com>
2014-05-23 21:23:56 +0200

    repair view #index page

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -81,6 +81,6 @@ class GistsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def gist_params
-    params.require(:gist).permit(:snippet, :lang, :description, :autor)
+    params.require(:gist).permit(:snippet, :lang, :description)
   end
 end
